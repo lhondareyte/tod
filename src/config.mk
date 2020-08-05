7a16576eaac2e09725be635d3ee6176f49043b8f
Luc Hondareyte <luc.hondareyte@laposte.net>
2020-08-05 09:36:17 +0200

    Set prescaler for external clock.

--- src/config.mk
+++ src/config.mk
@@ -5,7 +5,8 @@
 
 FIRMWARE	:=top-octave
 MCU		:=attiny13
-HZ		:=2000000
+#MCU		:=attiny4
+HZ		:=2000000 # To avoid warning only
 #
 # Valeurs de hfuse et lfuse
 LFUSE		:=0xff
--- src/top-octave.c
+++ src/top-octave.c
@@ -1,6 +1,15 @@
 #include "top-octave.h"
 
 int main (void) {
+
+#if defined (__AVR_ATtiny13__) || defined (__AVR_ATtiny13a__)
+	CLKPR=0x00;        // no prescaler (1:1)
+#endif
+
+#if defined __AVR_ATtiny4__
+	CLKMSR=0b00000010; // external clock
+	CLKPSR=0x00;       // no prescaler (1:1)
+#endif
 	divider();
 	return 0;
 }
