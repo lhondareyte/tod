30f83a4ea813fdc47fa8c25e9115930662a8ecfe
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2020-08-30 13:33:47 +0200

    Almost works on atmega328p

--- src/genedivider.pl
+++ src/genedivider.pl
@@ -4,8 +4,7 @@
 #
 
 use strict;
-my $offset  = 5;           # I/O cost 5 cycles in loop -> see @footer
-my $count   = 8;           # Indentation loop count
+my $offset  = 4;           # I/O cost 4 cycles in loop -> see @footer
 my $divider = shift ;
 
 if ( not defined $divider ) {
@@ -17,37 +16,28 @@ my $header   = <<EOH;
 #include <avr/io.h>
 #define temp r17
 .global divider
+	
 divider:
-	ldi temp, 0b11111111
-	out PORTB, temp
-loop:
+
 EOH
 
 my $footer = <<EOF;
-	
+
 	; Toggle output
-	subi temp, 1    ; 2 cycles
-	out PORTB, temp ; 1 cycles
-	rjmp loop       ; 2 cycles
+	; http:
+	ldi temp, 0xff              ; 1 cycle
+	out _SFR_IO_ADDR(PINB),temp ; 1 cycle
+	rjmp divider                ; 2 cycles
 EOF
 
 print STDOUT "$header";
 
 #
 # nop generation
-$divider = $divider - $offset;
+$divider = $divider - $offset + 1;
 while  ($divider) {
-	if ( $count % 8 ) {
-		print STDOUT ("nop; ");
-	}
-	else {
-		print STDOUT ("\n\tnop; ");
-	}
+	print STDOUT ("\tnop\n");
 	$divider = $divider - 1;
-	$count = $count - 1;
-	if ( $count == 0 ) {
-		$count = 8;
-	}
 }
 
 print STDOUT "$footer";
