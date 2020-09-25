b5d9b1efac47b1f734394541e812fd8bf6707e15
Luc Hondareyte <luc.hondareyte@laposte.net>
2020-09-25 11:54:36 +0200

    Specific prescaler only for attiny4

--- src/top-octave.c
+++ src/top-octave.c
@@ -6,13 +6,11 @@
 
 int main (void) {
 
-#if defined (__AVR_ATtiny13__) || defined (__AVR_ATtiny13a__)
-	CLKPR=0x00;        // no prescaler (1:1)
-#endif
-
 #if defined __AVR_ATtiny4__
 	CLKMSR=0b00000010; // external clock
 	CLKPSR=0x00;       // no prescaler (1:1)
+#else
+	CLKPR=0x00;        // no prescaler (1:1)
 #endif
 	DDRB=0xff;
 	divider();
