d391b4f217e617effc949450f746d113e5151be1
Luc Hondareyte <luc.hondareyte@laposte.net>
2020-09-25 11:54:51 +0200

    Using wait loop to reduce text size for attiny.

--- src/genedivider.pl
+++ src/genedivider.pl
@@ -7,8 +7,14 @@
 
 use warnings;
 use strict;
-my $offset  = 4;           # I/O cost 4 cycles in loop -> see @footer
+my $cost  = 5;               # I/O cost 4 cycles in loop -> see @footer
+                             # ldi cost 1 cycle
+
+my $loop = 20;               # wait loop = 20 cycles
 my $divider = shift ;
+my $counter = int(( $divider - $cost ) / $loop ) ;
+my $nop = $divider - ( $counter * $loop ) - $cost + 1 ; 
+#my $nop = $divider - ( $counter * 2 ) - $cost - 1 ; 
 
 if ( not defined $divider ) {
 	print "Usage : genedivider.pl <num> \n";
@@ -18,9 +24,37 @@ if ( not defined $divider ) {
 my $header   = <<EOH;
 #include <avr/io.h>
 #define temp r17
+#define counter r16
 .global divider
-	
+
 divider:
+	; 1 cycle
+	ldi counter, $counter 
+
+wait:
+	; counter x 20 cycles
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	nop
+	nop
+	nop
+
+	nop
+	nop
+	dec counter          
+	brne wait
 
 EOH
 
@@ -31,16 +65,19 @@ my $footer = <<EOF;
 	ldi temp, 0xff              ; 1 cycle
 	out _SFR_IO_ADDR(PINB),temp ; 1 cycle
 	rjmp divider                ; 2 cycles
+
 EOF
 
 print STDOUT "$header";
 
+#my $total = $nop + 5 + ($counter * 20);
+#print STDERR "Total cycles : $total - Padding : $nop \n";
 #
 # nop generation
-$divider = $divider - $offset + 1;
-while  ($divider) {
+print STDOUT ("\t; nop padding ($nop nops)\n");
+while  ($nop) {
 	print STDOUT ("\tnop\n");
-	$divider = $divider - 1;
+	$nop = $nop - 1;
 }
 
 print STDOUT "$footer";
