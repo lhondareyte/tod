6900f8e72a193416545eb69a15c83d4620ee6b83
Luc Hondareyte <luc.hondareyte@laposte.net>
2021-08-07 10:39:19 +0200

    Adding a config file per mcu

---
+++ src/Mk/atmega328.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2018 Luc HONDAREYTE
+# All rights reserved.
+#
+HZ	= 8000000 # To avoid warning only
+LFUSE	= 0xff
+HFUSE	= 0xdf
+
--- src/config.mk
+++ src/config.mk
@@ -7,9 +7,5 @@ FIRMWARE	:=top-octave
 MCU		:=atmega328
 #MCU		:=attiny13
 #MCU		:=attiny4
-HZ		:=8000000 # To avoid warning only
-#
-# Valeurs de hfuse et lfuse
-LFUSE		:=0xff
-HFUSE		:=0xdf
+include Mk/$(MCU).mk
 
